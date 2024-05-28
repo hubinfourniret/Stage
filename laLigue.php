93d8a0f02648ac5a0828b05809768bc98384ec8e
hubinfourniret <hubin.fourniret@sts-sio-caen.info>
2024-05-28 16:32:56 +0200

    123

--- laLigue.php
+++ laLigue.php
@@ -2,13 +2,13 @@
     include('header.php');
 ?>
         <main class="ligue">
-            <p class='text'>Association régie par la loi 1901, la Ligue de Normandie est l'organe décentralisé de la Fédération Française des Sports de Glace.</p>
+            <p class='text'>Association régie par la loi 1901, la <b>L</b>igue de <b>N</b>ormandie est l'organe décentralisé de la Fédération Française des <b>S</b>ports de <b>G</b>lace.</p>
             <img class="organigramme"src="photos/club/organigramme.jpg"/>
             <div class="card-reglement">
                 <div class="card-content">
                     <h2>Règlement de la Ligue</h2>
                     <p>Assurez-vous de lire le règlement complet pour connaître les règles et les directives.</p>
-                    <a href="reglement.pdf" class="btn-reglement" target="_blank">Consulter le règlement</a>
+                    <a href="documents/laLigue/REGLEMENT INTERIEUR LNSG  24.06.2017.pdf" class="btn-reglement" target="_blank">Consulter le règlement</a>
                 </div>
             </div>
             <p class='text'>Son siège situé dans l'Eure, elle regroupe sept disciplines, sept clubs répartis dans trois départements.</p>
--- lesClubs.php
+++ lesClubs.php
@@ -2,7 +2,10 @@
     include('header.php');
 ?>
         <main class="clubs">
+            <h1>LES CLUBS</h1>
             <div>
+                <img class="logo"src="logo/logo-club/acsel-logo.jpg"/>
+                <div>
                 <h1>ACSEL Caen patinage</h1>
                 <p>8, rue Jean de la Varende 14000 Caen</p>
                 <p>TEL 09.81.40.20.20</p>
@@ -10,6 +13,7 @@
                 <p>contact@acsel-caen-patinage.fr</p>
                 <p>PRESIDENT</p>
                 <p>LE GUENNEC Ludovic</p>
+                </div>
             </div>
         </main>
 <?php
